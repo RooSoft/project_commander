c000b47b7bc15e3a0e6a8e472c75fad58d40197b
Marc Lacoursiere <marc@roosoft.com>
2024-01-03 11:41:58 -0500

    #3 extracted the EventHandler::create_handler

--- src/tui/event.rs
+++ src/tui/event.rs
@@ -40,38 +40,8 @@ impl EventHandler {
     pub fn new(tick_rate: u64) -> Self {
         let tick_rate = Duration::from_millis(tick_rate);
         let (sender, receiver) = mpsc::channel();
-        let handler = {
-            let sender = sender.clone();
-            thread::spawn(move || {
-                let mut last_tick = Instant::now();
-                loop {
-                    let timeout = tick_rate
-                        .checked_sub(last_tick.elapsed())
-                        .unwrap_or(tick_rate);
+        let handler = Self::create_handler(sender.clone(), tick_rate);
 
-                    if event::poll(timeout).expect("unable to poll for event") {
-                        match event::read().expect("unable to read event") {
-                            CrosstermEvent::Key(e) => {
-                                if e.kind == event::KeyEventKind::Press {
-                                    sender.send(Event::Key(e))
-                                } else {
-                                    Ok(()) // ignore KeyEventKind::Release on windows
-                                }
-                            }
-                            CrosstermEvent::Mouse(e) => sender.send(Event::Mouse(e)),
-                            CrosstermEvent::Resize(w, h) => sender.send(Event::Resize(w, h)),
-                            _ => unimplemented!(),
-                        }
-                        .expect("failed to send terminal event")
-                    }
-
-                    if last_tick.elapsed() >= tick_rate {
-                        sender.send(Event::Tick).expect("failed to send tick event");
-                        last_tick = Instant::now();
-                    }
-                }
-            })
-        };
         Self {
             sender,
             receiver,
@@ -85,5 +55,37 @@ impl EventHandler {
     /// there is no data available and it's possible for more data to be sent.
     pub fn next(&self) -> Result<Event> {
         Ok(self.receiver.recv()?)
+    }
+
+    pub fn create_handler(sender: mpsc::Sender<Event>, tick_rate: Duration) -> thread::JoinHandle<()> {
+        thread::spawn(move || {
+            let mut last_tick = Instant::now();
+            loop {
+                let timeout = tick_rate
+                    .checked_sub(last_tick.elapsed())
+                    .unwrap_or(tick_rate);
+
+                if event::poll(timeout).expect("unable to poll for event") {
+                    match event::read().expect("unable to read event") {
+                        CrosstermEvent::Key(e) => {
+                            if e.kind == event::KeyEventKind::Press {
+                                sender.send(Event::Key(e))
+                            } else {
+                                Ok(()) // ignore KeyEventKind::Release on windows
+                            }
+                        }
+                        CrosstermEvent::Mouse(e) => sender.send(Event::Mouse(e)),
+                        CrosstermEvent::Resize(w, h) => sender.send(Event::Resize(w, h)),
+                        _ => unimplemented!(),
+                    }
+                    .expect("failed to send terminal event")
+                }
+
+                if last_tick.elapsed() >= tick_rate {
+                    sender.send(Event::Tick).expect("failed to send tick event");
+                    last_tick = Instant::now();
+                }
+            }
+        })
     }
 }
