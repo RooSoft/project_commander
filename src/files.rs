0564f34c0abb6cb580994ff6beade7761e8eb613
Marc Lacoursiere <marc@roosoft.com>
2024-01-03 09:48:49 -0500

    extracted the filter_folders function

--- src/files.rs
+++ src/files.rs
@@ -1,40 +1,43 @@
 use crate::git;
+use git2::Repository;
 use std::fs;
 use std::io;
 
 pub fn list_folders(parent: &str) -> Result<Vec<(String, git2::Repository)>, io::Error> {
     let repositories = fs::read_dir(parent)?
         .filter(keep_folders)
-        .filter_map(|e| {
-            let path_buf = &e.unwrap().path();
-            let path = path_buf.as_path();
-            let name = path.file_name().unwrap().to_str().unwrap();
-
-            if name != ".git" && name != "target" && name != ".." {
-                let relative_path = format!("{}/{}", parent, name);
-
-                match git::get_repository(&relative_path) {
-                    Some(repository) => Some(vec![(relative_path, repository)]),
-                    None => match list_folders(&relative_path) {
-                        Ok(repos) => Some(repos),
-                        Err(_) => None,
-                    },
-                }
-            } else {
-                None
-            }
-        })
+        .filter_map(|e| filter_folders(e, parent))
         .flatten()
         .collect::<Vec<(String, git2::Repository)>>();
 
     Ok(repositories)
 }
 
+fn filter_folders(e: Result<fs::DirEntry, io::Error>, parent: &str) -> Option<Vec<(String, Repository)>> {
+    let path_buf = &e.unwrap().path();
+    let path = path_buf.as_path();
+    let name = path.file_name().unwrap().to_str().unwrap();
+
+    if name != ".git" && name != "target" && name != ".." {
+        let relative_path = format!("{}/{}", parent, name);
+
+        match git::get_repository(&relative_path) {
+            Some(repository) => Some(vec![(relative_path, repository)]),
+            None => match list_folders(&relative_path) {
+                Ok(repos) => Some(repos),
+                Err(_) => None,
+            },
+        }
+    } else {
+        None
+    }
+}
+
 fn keep_folders(e: &Result<fs::DirEntry, io::Error>) -> bool {
     match e {
         Ok(entry) => match entry.metadata().map(|m| m.is_dir()) {
             Ok(result) => result,
-            Err(_) => false
+            Err(_) => false,
         },
         Err(_) => false,
     }
