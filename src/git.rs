f852cb282645d143be5065255584ca6a9f342cf2
Marc Lacoursiere <marc@roosoft.com>
2024-01-03 11:32:38 -0500

    #4 removed unwraps in git.rs

--- src/git.rs
+++ src/git.rs
@@ -4,52 +4,56 @@ struct File {
     path: String,
     is_new: bool,
     is_ignored: bool,
-} 
+}
 
 pub fn get_repository(folder: &str) -> Option<Repository> {
     match Repository::open(folder) {
         Ok(repository) => Some(repository),
-        Err(_) => None
+        Err(_) => None,
     }
 }
 
 pub fn list_files(folder: &str) {
     let x = match Repository::open(folder) {
-        Ok(repo) => repo
-            .statuses(None)
-            .unwrap()
-            .iter()
-            .map(convert_to_file)
-            .map(display_file)
-            .collect::<Vec<_>>()
-            .join("\n"),
+        Ok(repo) => match repo.statuses(None) {
+            Ok(statuses) => statuses
+                .iter()
+                .filter_map(convert_to_file)
+                .map(display_file)
+                .collect::<Vec<_>>()
+                .join("\n"),
+            Err(e) => format!("{}", e),
+        },
         Err(e) => format!("{}", e),
     };
 
     println!("{}", x);
 }
 
-fn convert_to_file(status_entry: StatusEntry) -> File {
-    let path = status_entry.path().unwrap();
-    let status = status_entry.status();
+fn convert_to_file(status_entry: StatusEntry) -> Option<File> {
+    if let Some(path) = status_entry.path() {
+        let status = status_entry.status();
 
-    let is_new = status.is_wt_new();
-    let is_ignored = status.is_ignored();
+        let is_new = status.is_wt_new();
+        let is_ignored = status.is_ignored();
 
-    File{path: path.to_string(), is_new, is_ignored}
+        Some(File {
+            path: path.to_string(),
+            is_new,
+            is_ignored,
+        })
+    } else {
+        None
+    }
 }
 
 fn display_file(file: File) -> String {
     let is_new = match file.is_new {
         true => "*NEW*",
-        false => ""
+        false => "",
     };
 
-    let ignored_status = if file.is_ignored {
-        "*IGNORED*"
-    } else {
-        ""
-    };
+    let ignored_status = if file.is_ignored { "*IGNORED*" } else { "" };
 
     format!("{} {} {}", file.path, is_new, ignored_status)
 }
