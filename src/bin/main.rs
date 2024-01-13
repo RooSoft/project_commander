8ecaee2843be264e7457f440f5925392fe5e7952
Marc Lacoursiere <marc@roosoft.com>
2024-01-13 14:11:41 -0500

    return . if nothing is selected

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -3,12 +3,12 @@ use project_commander::{configuration::Configuration, terminal_ui::app::App};
 use color_eyre::Result;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    dbg!(std::env::var("HOME"))?;
-
     let configuration = get_configuration();
 
     if let Some(output) = App::run(configuration.parent_folder())? {
         println!("{}", output);
+    } else {
+        println!(".");
     }
 
     Ok(())
