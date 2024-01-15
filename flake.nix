5979c42993704e98fe04ea7a8b79994ca57cb6e4
Marc Lacoursiere <marc@roosoft.com>
2024-01-15 13:46:57 -0500

    removed useless comments

--- flake.nix
+++ flake.nix
@@ -1,11 +1,6 @@
 {
   description = "Project Commander";
 
-  # outputs = { self, nixpkgs }: {
-  #   packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-  #   packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-  # };
-
   inputs = { 
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; 
