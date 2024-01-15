b9bf1572cb6296b41a680a2c1bc56078e7491266
Marc Lacoursiere <marc@roosoft.com>
2024-01-15 10:12:59 -0500

    updated the sha-256 hash

--- default.nix
+++ default.nix
@@ -5,7 +5,7 @@
       owner = "roosoft";
       repo = "project_commander";
       rev = "3cc282755f23c5722065dd98b2f94bcf13d61bcc";
-      sha256 = "sha256-2PoOPc2zIRA6jPESDfmza33n9AaOlO9luoRdozBlFqI=";
+      sha256 = "sha256-eh1MCcR18Fn0lWMgjVzbflJsjDaxNheuVdhsNoZOdUs=";
     };
 
     nativeBuildInputs = with pkgs; [ pkg-config ];
