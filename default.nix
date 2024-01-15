c670643098197e6442d71e9df09fb9e023729093
Marc Lacoursiere <marc@roosoft.com>
2024-01-15 09:54:07 -0500

    get sources from github in default.nix

--- default.nix
+++ default.nix
@@ -1,23 +1,13 @@
 { pkgs, nixpkgs, flake-utils, naersk' }:
 
-# pkgs.stdenv.mkDerivation {
-#   name = "Project Commander";  
-#   src = ./.;
-#
-#   buildInputs = [ pkgs.rustc pkgs.cargo ];
-#
-#   buildPhase = ''
-#     cargo build
-#   '';
-#
-#   installPhase = ''
-#     mkdir -p $out/bin
-#     cp $src/target/debug/main $out/bin
-#   '';
-# }
-naersk'.buildPackage {
-  src = ./.;
+  naersk'.buildPackage {
+    src = pkgs.fetchFromGitHub {
+      owner = "roosoft";
+      repo = "project_commander";
+      rev = "58c81f52d922e4b4c972544637befe1eafff0ecd";
+      sha256 = "sha256-2PoOPc2zIRA6jPESDfmza33n9AaOlO9luoRdozBlFqI=";
+    };
 
-  nativeBuildInputs = with pkgs; [ pkg-config ];
-  buildInputs = with pkgs; [ openssl ];
-}
+    nativeBuildInputs = with pkgs; [ pkg-config ];
+    buildInputs = with pkgs; [ openssl ];
+  }
