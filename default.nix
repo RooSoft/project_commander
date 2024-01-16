208b4098679c33a709c1a7c9d8ce13202a43a53b
Marc Lacoursiere <marc@roosoft.com>
2024-01-16 15:55:16 -0500

    reindentation

--- default.nix
+++ default.nix
@@ -1,13 +1,12 @@
 { pkgs, naersk' }:
+naersk'.buildPackage {
+  src = pkgs.fetchFromGitHub {
+    owner = "roosoft";
+    repo = "project_commander";
+    rev = "3cc282755f23c5722065dd98b2f94bcf13d61bcc";
+    sha256 = "sha256-eh1MCcR18Fn0lWMgjVzbflJsjDaxNheuVdhsNoZOdUs=";
+  };
 
-  naersk'.buildPackage {
-    src = pkgs.fetchFromGitHub {
-      owner = "roosoft";
-      repo = "project_commander";
-      rev = "3cc282755f23c5722065dd98b2f94bcf13d61bcc";
-      sha256 = "sha256-eh1MCcR18Fn0lWMgjVzbflJsjDaxNheuVdhsNoZOdUs=";
-    };
-
-    nativeBuildInputs = with pkgs; [ pkg-config ];
-    buildInputs = with pkgs; [ openssl ];
-  }
+  nativeBuildInputs = with pkgs; [ pkg-config ];
+  buildInputs = with pkgs; [ openssl ];
+}
