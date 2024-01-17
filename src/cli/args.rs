b561412f67d78ee208fed307a933e70a70e0ff9c
Marc Lacoursiere <marc@roosoft.com>
2024-01-17 13:14:41 -0500

    supplying the project name as a const

--- src/cli/args.rs
+++ src/cli/args.rs
@@ -1,8 +1,10 @@
 use clap::Parser;
 
+const NAME: &str = "Project Commander";
+
 /// Project Commander  -  Find git projects with ease on local storage
 #[derive(Parser, Debug)]
-#[command(author, version, about, name = "Project Commander", long_about = None)]
+#[command(author, version, about, name = NAME, long_about = None)]
 pub struct Args {
     /// Name of a project to search for
     #[arg(short, long, default_value = None)]
