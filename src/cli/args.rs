f45958cc7abdc249c3a5416102255ae0710c86ee
Marc Lacoursiere <marc@roosoft.com>
2024-01-17 13:17:11 -0500

    provided an about to clap

--- src/cli/args.rs
+++ src/cli/args.rs
@@ -1,10 +1,11 @@
 use clap::Parser;
 
 const NAME: &str = "Project Commander";
+const ABOUT: &str = "Project Commander -- Recursively find a project in a given parent folder";
 
 /// Project Commander  -  Find git projects with ease on local storage
 #[derive(Parser, Debug)]
-#[command(author, version, about, name = NAME, long_about = None)]
+#[command(author, version, about = ABOUT, name = NAME, long_about = None)]
 pub struct Args {
     /// Name of a project to search for
     #[arg(short, long, default_value = None)]
